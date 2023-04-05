201064245c22dd324faaf31a64268922b9fbd5a1
周雅琪 <zhouyaqi@zhouyaqideMacBook-Air.local>
2023-04-05 17:32:48 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+toolkit/                  <-- PyCharm project folder
+|
+|__ toolkit_config.py     <-- Configuration module
+|__ yf_example2.py        <-- You have already created this module
+|__ yf_example3.py        <-- The new module
+|__ data/                 <-- Folder where the data will be saved
